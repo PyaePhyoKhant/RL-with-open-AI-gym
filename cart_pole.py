8a15777ddd236ddaced8c1ae49983768577bf59d
Pyae Phyo Khant <lanmadawsmith@gmail.com>
2018-06-21 17:03:20 +0630

    simple control
    choose left or right according to whether angle is positive or negative

--- cart_pole.py
+++ cart_pole.py
@@ -3,9 +3,11 @@ import gym.spaces
 import time
 from quantizer import Quantizer
 
-
-dist_qtz = Quantizer(-2.4, 2.4, 1700)   # 1700 bins so that -0.14 to 0.14 have 100 bins
+dist_qtz = Quantizer(-2.4, 2.4, 1700)  # 1700 bins so that -0.14 to 0.14 have 100 bins
 ang_qtz = Quantizer(-0.20944, 0.20944, 100)  # -12 to 12 degree is -0.20944 to 0.20944 in radians
+
+(dist, v1, ang, v2) = (0, 0, 0, 0)
+
 env = gym.make('CartPole-v0')
 for i_episode in range(1):
     observation = env.reset()
@@ -13,9 +15,12 @@ for i_episode in range(1):
     for t in range(1000):
         env.render()
         print(observation)
-        observation, reward, done, info = env.step(env.action_space.sample())
+        if ang > 0:
+            observation, reward, done, info = env.step(1)
+        else:
+            observation, reward, done, info = env.step(0)
         (dist, v1, ang, v2) = observation
-        # time.sleep(0.1)
+        time.sleep(0.1)
         total_reward += reward
         if done:
             print("Episode finished after {} timesteps. Reward: {}".format(t + 1, total_reward))
