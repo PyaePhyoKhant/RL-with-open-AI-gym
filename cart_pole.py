74e4385079387afebea9ed0a66d302dc8058c901
Pyae Phyo Khant <lanmadawsmith@gmail.com>
2018-06-22 14:18:22 +0630

    refactor code and also consider cart velocity and tip velocity for state

--- cart_pole.py
+++ cart_pole.py
@@ -8,15 +8,20 @@ from q_learning import QLearningAgent
 # important global parameters
 MAX_DIST = 2.5
 MAX_RAD = 0.3
-LEARNING_EPISODES = 30000
+MAX_CART_VEL = 3.5
+MAX_TIP_VEL = 3.5
+LEARNING_EPISODES = 10000
 TESTING_EPISODES = 100
 LEARNING_RATE = 0.2
 DISCOUNT = 0.8
 EXPLORATION = 0.2
+BINS = 300
 
-ang_qtz = Quantizer(-MAX_RAD, MAX_RAD, 1000)  # -12 to 12 degree is -0.20944 to 0.20944 in radians
+ang_qtz = Quantizer(-MAX_RAD, MAX_RAD, BINS)  # -12 to 12 degree is -0.20944 to 0.20944 in radians
+cart_qtz = Quantizer(-MAX_CART_VEL, MAX_CART_VEL, BINS)
+tip_qtz = Quantizer(-MAX_TIP_VEL, MAX_TIP_VEL, BINS)
 
-(dist, v1, ang, v2) = (0, 0, 0, 0)
+(dist, cart_vel, ang, tip_vel) = (0, 0, 0, 0)
 
 env = gym.make('CartPole-v0')
 learner = QLearningAgent(env, LEARNING_RATE, DISCOUNT, EXPLORATION)
@@ -27,31 +32,28 @@ def extract_state(obs):
     extract state via this function so that it is DRY
     :param obs: gym observation
     """
-    (dist, v1, ang, v2) = obs
-    return ang
+    (dist, cart_vel, ang, tip_vel) = obs
+    ang = ang_qtz.round(ang)
+    cart_vel = cart_qtz.round(cart_vel)
+    tip_vel = tip_qtz.round(tip_vel)
+    return ang, cart_vel, tip_vel
 
 # Learning
 reward_list = []
-for i_episode in range(LEARNING_EPISODES):
+for _ in range(LEARNING_EPISODES):
     env.reset()
     total_reward = 0
     for _ in range(1000):
         # get action
-        ang = ang_qtz.round(ang)
-        old_state = extract_state((dist, v1, ang, v2))
-        if i_episode > 5000:
-            a = 1
+        old_state = extract_state((dist, cart_vel, ang, tip_vel))
         action = learner.get_action(old_state)
 
         # one step
         observation, reward, done, info = env.step(action)
-        (dist, v1, ang, v2) = observation
-        ang = ang_qtz.round(ang)
-        next_state = extract_state((dist, v1, ang, v2))
+        (dist, cart_vel, ang, tip_vel) = observation
+        next_state = extract_state((dist, cart_vel, ang, tip_vel))
 
         # update learner
-        if i_episode > 5000:
-            a = 1
         learner.update(old_state, action, next_state, reward)
 
         total_reward += reward
@@ -71,15 +73,13 @@ for _ in range(TESTING_EPISODES):
         # env.render()
 
         # get action
-        ang = ang_qtz.round(ang)
-        old_state = extract_state((dist, v1, ang, v2))
+        old_state = extract_state((dist, cart_vel, ang, tip_vel))
         action = learner.get_action(old_state)
 
         # one step
         observation, reward, done, info = env.step(action)
-        (dist, v1, ang, v2) = observation
-        ang = ang_qtz.round(ang)
-        next_state = extract_state((dist, v1, ang, v2))
+        (dist, cart_vel, ang, tip_vel) = observation
+        next_state = extract_state((dist, cart_vel, ang, tip_vel))
 
         # time.sleep(0.1)
         total_reward += reward
